7ad2a18c760a69097932bb68e118b13688b95884
살려주세요 <46598063+qwreey75@users.noreply.github.com>
2021-06-19 16:52:16 +0900

    Update render.lua

--- libs/termRBLX/render/term/render.lua
+++ libs/termRBLX/render/term/render.lua
@@ -1,42 +1,47 @@
 local module = {};
 
 local function new(ClassName,prop)
-    local new = Instance.new(ClassName);
-    for index,value in pairs(prop) do
-        local valueType = typeof(value);
-        local indexType = typeof(index);
+	local new = Instance.new(ClassName);
+	for index,value in pairs(prop) do
+		local valueType = typeof(value);
+		local indexType = typeof(index);
 
-        -- child
-        if indexType ~= "string" and valueType == "Instance" then
-            value.Parent = new;
-        elseif valueType == "function" then
-            -- connect event
-            if index ~= "whenCreated" then
-                new[index]:Connect(value);
-            end
-        elseif indexType == "string" then
-            -- property
-            new[index] = value
-        end
-    end
-    local whenCreated = prop["whenCreated"];
-    if whenCreated then
-        whenCreated(new);
-    end
-    return new;
+		-- child
+		if indexType ~= "string" and valueType == "Instance" then
+			value.Parent = new;
+		elseif valueType == "function" then
+			-- connect event
+			if index ~= "whenCreated" then
+				new[index]:Connect(value);
+			end
+		elseif indexType == "string" then
+			-- property
+			new[index] = value
+		end
+	end
+	local whenCreated = prop["whenCreated"];
+	if whenCreated then
+		whenCreated(new);
+	end
+	return new;
 end
 
+local storage = {};
+module.Storage = {};
+
 function module.Import(ClassName)
-    return function (prop)
-        if type(prop) == "string" then
-            local lastName = prop;
-            return function (nprop)
-                nprop.Name = lastName;
-                return new(ClassName,nprop);
-            end;
-        end
-        return new(ClassName,prop);
-    end;
+	return function (prop)
+		if type(prop) == "string" then
+			local lastName = prop;
+			return function (nprop)
+				nprop.Name = lastName;
+				local item = new(ClassName,nprop);
+				storage[lastName] = item;
+				return item;
+			end;
+		end
+		return new(ClassName,prop);
+	end;
 end
 
 return module;
